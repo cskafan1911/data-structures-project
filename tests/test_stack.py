092431132fe321effeb83e70757ba21cff4bee2b
Dmitry Dronov <cskafan1911@gmail.com>
2023-08-19 10:08:30 +0300

    Написан тест для класса Stack

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,18 @@
-"""Здесь надо написать тесты с использованием unittest для модуля stack."""
+import unittest
+from src.stack import Stack, Node
+
+
+class TestStack(unittest.TestCase):
+
+    """
+    Тесты для класса Stack
+    """
+
+    def setUp(self):
+        self.stack = Stack()
+
+    def test_push(self):
+        assert self.stack.size == 0
+        self.stack.push(10)
+        assert self.stack.size == 1
+        assert self.stack.top.data == 10
